7b01adbaf4ae28f497036c5f78a4b7abd7889ac4
DhiaRomd <dhia.romdhane@clevertech-france.fr>
2025-04-24 21:58:35 +0100

    Update .env file and add new migrats

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-class RedirectController extends AbstractController
+class MainController extends AbstractController
 {
     #[Route('/', name: 'redirect_to_login')]
     public function index(): RedirectResponse
